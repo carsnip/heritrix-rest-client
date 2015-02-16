4edd41453689761975b7519836497d7700d8e4f7
simon <simon@carsnip.com>
2015-02-16 09:54:57 +0000

    added tostring, hash code and equals.

--- src/main/java/com/github/truemped/heritrix/JobStatusReport.java
+++ src/main/java/com/github/truemped/heritrix/JobStatusReport.java
@@ -232,6 +232,345 @@ public class JobStatusReport {
 	public String getLastReachedState() {
 		return lastReachedState;
 	}
+	
+	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((activeQueues == null) ? 0 : activeQueues.hashCode());
+		result = prime
+				* result
+				+ ((averageDocsPerSecond == null) ? 0 : averageDocsPerSecond
+						.hashCode());
+		result = prime
+				* result
+				+ ((averageKiBPerSec == null) ? 0 : averageKiBPerSec.hashCode());
+		result = prime
+				* result
+				+ ((averageQueueDepth == null) ? 0 : averageQueueDepth
+						.hashCode());
+		result = prime * result
+				+ ((busyThreads == null) ? 0 : busyThreads.hashCode());
+		result = prime * result
+				+ ((congestionRatio == null) ? 0 : congestionRatio.hashCode());
+		result = prime
+				* result
+				+ ((currentDocsPerSecond == null) ? 0 : currentDocsPerSecond
+						.hashCode());
+		result = prime
+				* result
+				+ ((currentKiBPerSec == null) ? 0 : currentKiBPerSec.hashCode());
+		result = prime
+				* result
+				+ ((deepestQueueDepth == null) ? 0 : deepestQueueDepth
+						.hashCode());
+		result = prime
+				* result
+				+ ((downloadedUriCount == null) ? 0 : downloadedUriCount
+						.hashCode());
+		result = prime * result
+				+ ((dupByHash == null) ? 0 : dupByHash.hashCode());
+		result = prime * result
+				+ ((dupByHashCount == null) ? 0 : dupByHashCount.hashCode());
+		result = prime
+				* result
+				+ ((elapsedMilliseconds == null) ? 0 : elapsedMilliseconds
+						.hashCode());
+		result = prime * result
+				+ ((elapsedPretty == null) ? 0 : elapsedPretty.hashCode());
+		result = prime * result
+				+ ((exhaustedQueues == null) ? 0 : exhaustedQueues.hashCode());
+		result = prime * result
+				+ ((futureUriCount == null) ? 0 : futureUriCount.hashCode());
+		result = prime * result
+				+ ((inProcessQueues == null) ? 0 : inProcessQueues.hashCode());
+		result = prime * result
+				+ ((inactiveQueues == null) ? 0 : inactiveQueues.hashCode());
+		result = prime
+				* result
+				+ ((ineligibleQueues == null) ? 0 : ineligibleQueues.hashCode());
+		result = prime * result + ((job == null) ? 0 : job.hashCode());
+		result = prime
+				* result
+				+ ((lastReachedState == null) ? 0 : lastReachedState.hashCode());
+		result = prime * result
+				+ ((notModified == null) ? 0 : notModified.hashCode());
+		result = prime
+				* result
+				+ ((notModifiedCount == null) ? 0 : notModifiedCount.hashCode());
+		result = prime * result + ((novel == null) ? 0 : novel.hashCode());
+		result = prime * result
+				+ ((novelCount == null) ? 0 : novelCount.hashCode());
+		result = prime * result
+				+ ((processors == null) ? 0 : processors.hashCode());
+		result = prime * result
+				+ ((queuedUriCount == null) ? 0 : queuedUriCount.hashCode());
+		result = prime * result
+				+ ((readyQueues == null) ? 0 : readyQueues.hashCode());
+		result = prime * result
+				+ ((retiredQueues == null) ? 0 : retiredQueues.hashCode());
+		result = prime * result
+				+ ((snoozedQueues == null) ? 0 : snoozedQueues.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		result = prime * result + ((steps == null) ? 0 : steps.hashCode());
+		result = prime * result
+				+ ((toeCount == null) ? 0 : toeCount.hashCode());
+		result = prime * result + ((total == null) ? 0 : total.hashCode());
+		result = prime * result
+				+ ((totalCount == null) ? 0 : totalCount.hashCode());
+		result = prime * result
+				+ ((totalQueues == null) ? 0 : totalQueues.hashCode());
+		result = prime * result
+				+ ((totalThreads == null) ? 0 : totalThreads.hashCode());
+		result = prime * result
+				+ ((totalUriCount == null) ? 0 : totalUriCount.hashCode());
+		result = prime
+				* result
+				+ ((warcWriterDirectory == null) ? 0 : warcWriterDirectory
+						.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		JobStatusReport other = (JobStatusReport) obj;
+		if (activeQueues == null) {
+			if (other.activeQueues != null)
+				return false;
+		} else if (!activeQueues.equals(other.activeQueues))
+			return false;
+		if (averageDocsPerSecond == null) {
+			if (other.averageDocsPerSecond != null)
+				return false;
+		} else if (!averageDocsPerSecond.equals(other.averageDocsPerSecond))
+			return false;
+		if (averageKiBPerSec == null) {
+			if (other.averageKiBPerSec != null)
+				return false;
+		} else if (!averageKiBPerSec.equals(other.averageKiBPerSec))
+			return false;
+		if (averageQueueDepth == null) {
+			if (other.averageQueueDepth != null)
+				return false;
+		} else if (!averageQueueDepth.equals(other.averageQueueDepth))
+			return false;
+		if (busyThreads == null) {
+			if (other.busyThreads != null)
+				return false;
+		} else if (!busyThreads.equals(other.busyThreads))
+			return false;
+		if (congestionRatio == null) {
+			if (other.congestionRatio != null)
+				return false;
+		} else if (!congestionRatio.equals(other.congestionRatio))
+			return false;
+		if (currentDocsPerSecond == null) {
+			if (other.currentDocsPerSecond != null)
+				return false;
+		} else if (!currentDocsPerSecond.equals(other.currentDocsPerSecond))
+			return false;
+		if (currentKiBPerSec == null) {
+			if (other.currentKiBPerSec != null)
+				return false;
+		} else if (!currentKiBPerSec.equals(other.currentKiBPerSec))
+			return false;
+		if (deepestQueueDepth == null) {
+			if (other.deepestQueueDepth != null)
+				return false;
+		} else if (!deepestQueueDepth.equals(other.deepestQueueDepth))
+			return false;
+		if (downloadedUriCount == null) {
+			if (other.downloadedUriCount != null)
+				return false;
+		} else if (!downloadedUriCount.equals(other.downloadedUriCount))
+			return false;
+		if (dupByHash == null) {
+			if (other.dupByHash != null)
+				return false;
+		} else if (!dupByHash.equals(other.dupByHash))
+			return false;
+		if (dupByHashCount == null) {
+			if (other.dupByHashCount != null)
+				return false;
+		} else if (!dupByHashCount.equals(other.dupByHashCount))
+			return false;
+		if (elapsedMilliseconds == null) {
+			if (other.elapsedMilliseconds != null)
+				return false;
+		} else if (!elapsedMilliseconds.equals(other.elapsedMilliseconds))
+			return false;
+		if (elapsedPretty == null) {
+			if (other.elapsedPretty != null)
+				return false;
+		} else if (!elapsedPretty.equals(other.elapsedPretty))
+			return false;
+		if (exhaustedQueues == null) {
+			if (other.exhaustedQueues != null)
+				return false;
+		} else if (!exhaustedQueues.equals(other.exhaustedQueues))
+			return false;
+		if (futureUriCount == null) {
+			if (other.futureUriCount != null)
+				return false;
+		} else if (!futureUriCount.equals(other.futureUriCount))
+			return false;
+		if (inProcessQueues == null) {
+			if (other.inProcessQueues != null)
+				return false;
+		} else if (!inProcessQueues.equals(other.inProcessQueues))
+			return false;
+		if (inactiveQueues == null) {
+			if (other.inactiveQueues != null)
+				return false;
+		} else if (!inactiveQueues.equals(other.inactiveQueues))
+			return false;
+		if (ineligibleQueues == null) {
+			if (other.ineligibleQueues != null)
+				return false;
+		} else if (!ineligibleQueues.equals(other.ineligibleQueues))
+			return false;
+		if (job == null) {
+			if (other.job != null)
+				return false;
+		} else if (!job.equals(other.job))
+			return false;
+		if (lastReachedState == null) {
+			if (other.lastReachedState != null)
+				return false;
+		} else if (!lastReachedState.equals(other.lastReachedState))
+			return false;
+		if (notModified == null) {
+			if (other.notModified != null)
+				return false;
+		} else if (!notModified.equals(other.notModified))
+			return false;
+		if (notModifiedCount == null) {
+			if (other.notModifiedCount != null)
+				return false;
+		} else if (!notModifiedCount.equals(other.notModifiedCount))
+			return false;
+		if (novel == null) {
+			if (other.novel != null)
+				return false;
+		} else if (!novel.equals(other.novel))
+			return false;
+		if (novelCount == null) {
+			if (other.novelCount != null)
+				return false;
+		} else if (!novelCount.equals(other.novelCount))
+			return false;
+		if (processors == null) {
+			if (other.processors != null)
+				return false;
+		} else if (!processors.equals(other.processors))
+			return false;
+		if (queuedUriCount == null) {
+			if (other.queuedUriCount != null)
+				return false;
+		} else if (!queuedUriCount.equals(other.queuedUriCount))
+			return false;
+		if (readyQueues == null) {
+			if (other.readyQueues != null)
+				return false;
+		} else if (!readyQueues.equals(other.readyQueues))
+			return false;
+		if (retiredQueues == null) {
+			if (other.retiredQueues != null)
+				return false;
+		} else if (!retiredQueues.equals(other.retiredQueues))
+			return false;
+		if (snoozedQueues == null) {
+			if (other.snoozedQueues != null)
+				return false;
+		} else if (!snoozedQueues.equals(other.snoozedQueues))
+			return false;
+		if (status == null) {
+			if (other.status != null)
+				return false;
+		} else if (!status.equals(other.status))
+			return false;
+		if (steps == null) {
+			if (other.steps != null)
+				return false;
+		} else if (!steps.equals(other.steps))
+			return false;
+		if (toeCount == null) {
+			if (other.toeCount != null)
+				return false;
+		} else if (!toeCount.equals(other.toeCount))
+			return false;
+		if (total == null) {
+			if (other.total != null)
+				return false;
+		} else if (!total.equals(other.total))
+			return false;
+		if (totalCount == null) {
+			if (other.totalCount != null)
+				return false;
+		} else if (!totalCount.equals(other.totalCount))
+			return false;
+		if (totalQueues == null) {
+			if (other.totalQueues != null)
+				return false;
+		} else if (!totalQueues.equals(other.totalQueues))
+			return false;
+		if (totalThreads == null) {
+			if (other.totalThreads != null)
+				return false;
+		} else if (!totalThreads.equals(other.totalThreads))
+			return false;
+		if (totalUriCount == null) {
+			if (other.totalUriCount != null)
+				return false;
+		} else if (!totalUriCount.equals(other.totalUriCount))
+			return false;
+		if (warcWriterDirectory == null) {
+			if (other.warcWriterDirectory != null)
+				return false;
+		} else if (!warcWriterDirectory.equals(other.warcWriterDirectory))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "JobStatusReport [job=" + job + ", status=" + status
+				+ ", warcWriterDirectory=" + warcWriterDirectory
+				+ ", downloadedUriCount=" + downloadedUriCount
+				+ ", queuedUriCount=" + queuedUriCount + ", totalUriCount="
+				+ totalUriCount + ", futureUriCount=" + futureUriCount
+				+ ", dupByHash=" + dupByHash + ", dupByHashCount="
+				+ dupByHashCount + ", notModified=" + notModified
+				+ ", notModifiedCount=" + notModifiedCount + ", novel=" + novel
+				+ ", novelCount=" + novelCount + ", total=" + total
+				+ ", totalCount=" + totalCount + ", currentDocsPerSecond="
+				+ currentDocsPerSecond + ", averageDocsPerSecond="
+				+ averageDocsPerSecond + ", currentKiBPerSec="
+				+ currentKiBPerSec + ", averageKiBPerSec=" + averageKiBPerSec
+				+ ", busyThreads=" + busyThreads + ", totalThreads="
+				+ totalThreads + ", congestionRatio=" + congestionRatio
+				+ ", averageQueueDepth=" + averageQueueDepth
+				+ ", deepestQueueDepth=" + deepestQueueDepth
+				+ ", elapsedMilliseconds=" + elapsedMilliseconds
+				+ ", elapsedPretty=" + elapsedPretty + ", toeCount=" + toeCount
+				+ ", steps=" + steps + ", processors=" + processors
+				+ ", totalQueues=" + totalQueues + ", inProcessQueues="
+				+ inProcessQueues + ", readyQueues=" + readyQueues
+				+ ", snoozedQueues=" + snoozedQueues + ", activeQueues="
+				+ activeQueues + ", inactiveQueues=" + inactiveQueues
+				+ ", ineligibleQueues=" + ineligibleQueues + ", retiredQueues="
+				+ retiredQueues + ", exhaustedQueues=" + exhaustedQueues
+				+ ", lastReachedState=" + lastReachedState + "]";
+	}
 
 	public static JobStatusReport fromDocument(Document document) {
 		final XPath xPath = XPathFactory.newInstance().newXPath();
